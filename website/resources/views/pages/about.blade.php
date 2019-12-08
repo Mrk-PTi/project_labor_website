a837475e9d2204e3d513bdf331e153e931b2dedf
Alaric <alaric.bell@gmail.com>
2019-12-08 01:52:48 +0100

    Adding about.blade.php to views/pages

---
+++ website/resources/views/pages/about.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('content')
+	<div class="container">
+		<h1>{{$title}}</h1>
+	    <p>Socialgram was created to provide its users an easy way to share their experiences / life events / thoughts with others and to help them get in touch with their frinds or make some new ones.</p>
+	    <h4>Creators:</h4>
+	    <ul>
+	    	<li>Szalai Márk</li>
+	    	<li>Bárdos Bálint</li>
+	    	<li>Molnár Dávid</li>
+	    </ul>
+	    <h4>Created for:</h4>
+	    <p>Eszterházy Károly University</p>
+	</div>
+@endsection
