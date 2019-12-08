ac74266965478f461ef0f130d4553590d3e86454
Alaric <alaric.bell@gmail.com>
2019-12-08 02:54:27 +0100

    updating dashboard page

--- website/resources/views/home.blade.php
+++ website/resources/views/home.blade.php
@@ -13,8 +13,32 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                    <div>
+                        <a href="/posts/create" class="btn btn-primary">Create Post</a>
+                        <h3>Your blog posts</h3>
+                        @if(count($posts) > 0)
+                            <table class="table">
+                                <tr>
+                                    <th>Title</th>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                                @foreach($posts as $post)
+                                    <tr>
+                                        <td>{{$post->title}}</td>
+                                        <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></td>
+                                        <td>{!!Form::open(['action' => ['PostController@destroy', $post->id], 'method' => 'POST', 'class' => 'ml-auto'])!!}
+                                                {{Form::hidden('_method', 'DELETE')}}
+                                                {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                                            {!!Form::close()!!}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </table>
+                            @else
+                                <p>You have no posts</p>
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
