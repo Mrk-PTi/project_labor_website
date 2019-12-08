775d0a9ced1aacb82796761a14c946af721074c2
Alaric <alaric.bell@gmail.com>
2019-12-08 02:57:06 +0100

    Updating edit.blade.php by adding file handling

--- website/resources/views/posts/edit.blade.php
+++ website/resources/views/posts/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <h1>Edit Post</h1>
-    {!! Form::open(['action' => ['PostController@update', $post->id], 'method' => 'POST']) !!}
+    {!! Form::open(['action' => ['PostController@update', $post->id], 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
         <div class="form-group">
             {{Form::label('title', 'Title')}}
             {{Form::text('title', {{$post->title}}, ['class' => 'form-control', 'placeholder' => 'Title'])}}
@@ -11,6 +11,9 @@
             {{Form::textarea('body', $post->body, ['class' => 'form-control', 'placeholder' => 'Body text'])}}
         </div>
         {{Form::hidden('_method', 'PUT')}}
+        <div class="form-group">
+            {{Form::file('cover_image')}}
+        </div>
         {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
     {!! Form::close() !!}
 @endsection
