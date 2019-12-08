2b181db0b26094e0e6c1ad6edbc2bf7785062acd
Alaric <alaric.bell@gmail.com>
2019-12-08 02:38:10 +0100

    Updating show.blade.php with authentication

--- website/resources/views/posts/show.blade.php
+++ website/resources/views/posts/show.blade.php
@@ -8,11 +8,15 @@
 	    	<hr>
 	    	<small class="card-subtitle mb-2 text-muted">Written on: {{$post->created_at}} by {{$post->user->name}}</small>
     		<hr>
-    		<a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
-    		{!!Form::open(['action' => ['PostController@destroy', $post->id], 'method' => 'POST', 'class' => 'ml-auto'])!!}
-    			{{Form::hidden('_method', 'DELETE')}}
-    			{{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-    		{!!Form::close()!!}
+    		@if(!Auth::guest())
+    			@if(Auth::user()->id == $post->user_id)
+		    		<a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+		    		{!!Form::open(['action' => ['PostController@destroy', $post->id], 'method' => 'POST', 'class' => 'ml-auto'])!!}
+		    			{{Form::hidden('_method', 'DELETE')}}
+		    			{{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+		    		{!!Form::close()!!}
+    			@endif
+    		@endif
 	  	</div>
 	</div>
 @endsection
