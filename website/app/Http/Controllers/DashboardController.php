5c9f56a3dc1c842a5492b6c1d9090a6f3f953502
Mrk-PTi <55917870+Mrk-PTi@users.noreply.github.com>
2019-12-10 22:37:35 +0100

    Adding index function to DashboardController.php

--- website/app/Http/Controllers/DashboardController.php
+++ website/app/Http/Controllers/DashboardController.php
@@ -10,6 +10,13 @@ class DashboardController extends Controller
 	public function __construct()
     {
         $this->middleware('auth');
-    }	
+    }
+	
+	public function index()
+    {
+        $user_id = auth()->user()->id;
+        $user = User::find($user_id);
+        return view('dashboard')->with('posts', $user->posts);
+    }
 }
 ?>
